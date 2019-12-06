ec4804d8f7f606c0c1893a242b20a86fb2b856d2
Federico Andrucci <federico.andrucci@gmail.com>
2019-12-06 16:53:41 +0100

    Update serverCringe.c

--- serverCringe.c
+++ serverCringe.c
@@ -1,1 +1,34 @@
+#include "fattoreX.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <rpc/rpc.h>
+
+static Candidati candidati;
+static int inizializzato = 0;
+
+void inizializza() {
+    int i;
+    int j;
+
+    if(inizializzato == 1) {
+        return;
+    }
+
+    for(i = 0; i < MAXCANDIDATI; i++) {
+        candidati.candidato[i]
+    } 
+}
+
+Output * classifica_giudici_1_svc(void *in, struct svc_req *rp) {
+
+
+
+}
+
+Candidati * esprimi_voto_1_svc(Input *in, struct svc_req *rp) {
+
+
+
+}
 
